f1d7726f05ea682c97a9acfbc14cf9ca115cac88
Eric Zorn <zornwebdev@gmail.com>
2021-06-27 13:10:52 -0400

    fix: initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Global Middleware
+  app.enableCors();
+
+  // Global Pipes
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const PORT = process.env.PORT ?? 8080;
+  await app.listen(PORT, () => {
+    Logger.debug(`Server started on port ${PORT}`);
+  });
 }
 bootstrap();
