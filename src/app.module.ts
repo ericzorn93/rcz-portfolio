8e1373dd5c079888b5c96c8d829601107472dbbc
Eric Zorn <zornwebdev@gmail.com>
2021-06-27 13:15:17 -0400

    feat: add config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SheetsModule } from './sheets/sheets.module';
 
 @Module({
-  imports: [SheetsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), SheetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
