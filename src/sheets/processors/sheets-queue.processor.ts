4e4fdff342f3866d8bb2ead68e8e50d7995e0df4
Eric Zorn <zornwebdev@gmail.com>
2021-07-05 12:39:48 -0400

    fix: fetching row ready for save in queue service

--- src/sheets/processors/sheets-queue.processor.ts
+++ src/sheets/processors/sheets-queue.processor.ts
@@ -1,4 +1,8 @@
-import { GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
+import {
+  GoogleSpreadsheetWorksheet,
+  GoogleSpreadsheetRow,
+} from 'google-spreadsheet';
+import { Logger, OnModuleInit } from '@nestjs/common';
 import {
   Processor,
   Process,
@@ -13,23 +17,40 @@ import { Job, JobId } from 'bull';
 
 import { IGoogleSheetQueueJobData } from './../types/sheetQueue.types';
 import { GoogleApisService } from '../services/google-apis/google-apis.service';
-import { OnModuleInit } from '@nestjs/common';
 
 @Processor('googleSheetStockRows')
 export class SheetsQueueConsumer implements OnModuleInit {
+  private readonly logger = new Logger('SheetsQueueConsumer');
   private sheet: GoogleSpreadsheetWorksheet;
+  private sheetRows: GoogleSpreadsheetRow[];
 
   constructor(private readonly googleApisService: GoogleApisService) {}
 
   public async onModuleInit(): Promise<void> {
+    const originalTime = Date.now();
+    this.logger.debug(`Starting Google Sheets Queue Connections`);
     this.sheet = await this.googleApisService.getFirstSheet();
+    this.sheetRows = await this.sheet.getRows();
+
+    // Loads all cells on initial module mount
+    await this.sheet.loadCells();
+
+    // Mark when queue and server are ready
+    const timePassed = Date.now() - originalTime;
+    this.logger.debug(`Google Sheets Queue Ready in time=${timePassed}ms`);
   }
 
   @Process()
   public async transcode(job: Job<IGoogleSheetQueueJobData>): Promise<JobId> {
     // Wait one second to process
-    // await new Promise(res => setTimeout(res, 1000));
-    console.log(this.sheet.sheetId, job.data);
+    // await new Promise(res => setTimeout(res, 1500));
+    const cell = await this.sheet.getCellByA1(job.data.a1Range);
+    const row = this.sheetRows[cell.rowIndex];
+
+    // Save updated row to google spreasheet
+    await row.save();
+
+    this.logger.log(`Row ${cell.a1Address} updated`);
 
     return job.id;
   }
@@ -41,28 +62,28 @@ export class SheetsQueueConsumer implements OnModuleInit {
 
   @OnQueueActive()
   public onActive(job: Job): void {
-    console.log(
+    this.logger.log(
       `Processing job ${job.id} of type ${job.name} with data ${job.data}...`,
     );
   }
 
   @OnQueueCleaned()
   public onClean(): void {
-    console.log('queue cleaned');
+    this.logger.log('queue cleaned');
   }
 
   @OnQueueStalled()
   public onStalled(): void {
-    console.log('stalled');
+    this.logger.log('stalled');
   }
 
   @OnQueuePaused()
   public onPause(): void {
-    console.log('Queue paused');
+    this.logger.log('Queue paused');
   }
 
   @OnQueueFailed()
   public fail(err: Error): void {
-    console.log('failure', err);
+    this.logger.log(err);
   }
 }
