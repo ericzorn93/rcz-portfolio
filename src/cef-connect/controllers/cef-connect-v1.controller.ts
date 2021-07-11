e3ca493adc6e6c60001d7ec2d55c6ca81a621d71
Eric Zorn <zornwebdev@gmail.com>
2021-07-11 14:26:43 -0400

    refactor: changing the fetching logic and added cef controller

---
+++ src/cef-connect/controllers/cef-connect-v1.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+import { CEFDailyPrice } from '../dto/cef.dailyPricing.response';
+import { CefConnectService } from '../services/cef-connect.service';
+
+@ApiTags('v1/cef-connect')
+@Controller('v1/cef-connect')
+export class CefConnectV1Controller {
+	constructor(private readonly cefConnectService: CefConnectService) {}
+
+		@ApiOkResponse({type: [CEFDailyPrice]})
+    @Get('/daily-prices')
+    public async cefConnectWelcome(): Promise<CEFDailyPrice[]> {
+        return this.cefConnectService.getClosedEndFundPricingArray();
+    }
+}
--- src/cef-connect/services/cef-connect.service.ts
+++ src/cef-connect/services/cef-connect.service.ts
@@ -1,4 +1,4 @@
-import { HttpService, Injectable, Logger } from '@nestjs/common';
+import { HttpService, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 
 import {
   CEFDailyPrice,
@@ -11,6 +11,20 @@ export class CefConnectService {
 
   constructor(private readonly httpService: HttpService) {}
 
+
+	/**
+	 * Returns the array of Closed End Fund daily pricing
+	 * for the current time stamp that the user has executed the query.
+	 *
+	 * @return {Promise<CEFDailyPrice[]>}  {Promise<CEFDailyPrice[]>}
+	 * @memberof CefConnectService
+	 */
+	public async getClosedEndFundPricingArray(): Promise<CEFDailyPrice[]> {
+		this.logger.debug(`date=${Date.now()} Obtaining current Closed End Fund Pricing data`)
+		return this.fetchCefConnectDailyPrices()
+	}
+
+
   /**
    * Obtains all stock information from CEF Connect,
    * specifically for closed end funds
@@ -22,11 +36,8 @@ export class CefConnectService {
     Record<string, CEFDailyPrice>
   > {
     try {
-      const time = Date.now();
 
-      const { data: dailyPrices } = await this.httpService
-        .get<CEFDailyPrice[]>(`/DailyPricing?&_=${time}`)
-        .toPromise();
+      const dailyPrices = await this.fetchCefConnectDailyPrices();
 
       return dailyPrices.reduce(
         (acc, stock) => ({
@@ -42,6 +53,32 @@ export class CefConnectService {
       return null;
     }
   }
+
+
+	/**
+	 * Makes API request to the CEF Connect Daily prices endpoint and returns an array of all closed end fund data
+	 * for the current timestamp
+	 *
+	 * @private
+	 * @return {Promise<CEFDailyPrice[]>}  {Promise<CEFDailyPrice[]>}
+	 * @memberof CefConnectService
+	 */
+	private async fetchCefConnectDailyPrices(): Promise<CEFDailyPrice[]> {
+		try {
+			const timeStart = Date.now()
+			const { data: dailyPrices } = await this.httpService
+		.get<CEFDailyPrice[]>(`/DailyPricing?&_=${timeStart}`)
+		.toPromise();
+		const timeElapsed = Date.now() - timeStart
+
+		this.logger.debug(`date=${Date.now()} Fetched the CEF Connect Daily Prices in ${timeElapsed}ms`)
+
+		return dailyPrices
+		} catch(err) {
+			this.logger.error(`date=${Date.now()} Having trouble fetching CEF Connect daily prices`)
+			throw new InternalServerErrorException('Cannot successfully fetch the Closed End Fund Daily Pricing. Please try again.')
+		}
+	}
 
   /**
    * Convert CEF data to proper type and shape for the Google Sheet
