529ef1203bc65c74805f63feb1dbde2a550fbba3
Eric Zorn <zornwebdev@gmail.com>
2021-07-31 12:59:18 -0400

    feat: adding public folder

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,47 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Global Middleware
-  app.enableCors();
+	// Global Middleware
+	app.enableCors();
 
-  // Global Pipes
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+	// Global Pipes
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // Swagger/OpenAPI Documentation
-  const config = new DocumentBuilder()
-    .setTitle('RCZ Portfolio Spreadsheet API')
-    .setDescription(
-      'A REST and GraphQL API that interacts with the RCZ Portfolio Google Sheet to pull in latest stock and closed-end fund data.',
-    )
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+	// Static Files
+	app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  // Start Server
-  const PORT = process.env.PORT ?? 8080;
-  await app.listen(PORT, () => {
-    Logger.debug(`Server started on port ${PORT}`);
-  });
+	// Swagger/OpenAPI Documentation and write swagger spec on startup
+	const config = new DocumentBuilder()
+		.setTitle('RCZ Portfolio Spreadsheet API')
+		.setDescription(
+			'A REST and GraphQL API that interacts with the RCZ Portfolio Google Sheet to pull in latest stock and closed-end fund data.',
+		)
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	try {
+		writeFileSync('./public/docs/swagger-spec.json', JSON.stringify(document));
+	} catch (err) {
+		console.log(err);
+
+		Logger.error(
+			`date=${Date.now()} Had trouble outputting schema to JSON file`,
+		);
+	}
+	SwaggerModule.setup('api-docs', app, document);
+
+	// Start Server
+	const PORT = process.env.PORT ?? 8080;
+	await app.listen(PORT, () => {
+		Logger.debug(`Server started on port ${PORT}`);
+	});
 }
 bootstrap();
